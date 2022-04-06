67f3d29eb8599c906ef99ad0d1cf8b3600d8358d
Ankur Khandelwal <khandelwalankur176@gmail.com>
2022-04-07 02:25:24 +0530

    added mock data

---
+++ src/_mocks_/tableData.js
@@ -0,0 +1,6 @@
+const events = ["Majestic Tours", "Crucible Games", "Lumen Prize", "Elitzer Events", "Annual Events", "Zenith Mania", "Cultural Crave"];
+const fields = ["Sports", "Fine Arts", "Performing Arts", "Literature", "Others", "Sports", "Fine Arts"];
+const scope = ["School", "Regional", "National", "International"];
+const notice = ["Final Exams", "New Time Table", "Results Updated", "Quiz Mania", "Fees Notice", "Debate Competition", "Prefect Selection"];
+
+export {events, fields, scope, notice};
